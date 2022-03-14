6497d71273fded45a61272ed7be81720b6a4bac1
Haroon <Haroon@LAPTOP-965P0DV9>
2022-03-14 07:09:42 +0530

    lap

---
+++ lapbook/src/lapbookassignment/AddTwo.java
@@ -0,0 +1,14 @@
+package lapbookassignment;
+
+public class AddTwo {
+	public static void main(String[] args) {
+		System.out.println("Enter the two numbers");
+		int first=100;
+		int second=200;
+		System.out.println(first+" "+second);
+		int sum=first+second;
+		System.out.println("The sum of the two is: "+sum);
+		
+	}
+
+}
